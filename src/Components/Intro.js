292da5b84fd45bc7d327057139e8b794d2b70850
Amber Srivastava <ambersrivastava19@gmail.com>
2023-07-31 21:18:13 +0530

    second time

--- src/Components/Intro.js
+++ src/Components/Intro.js
@@ -3,10 +3,10 @@ const Intro = () => {
     <div className="sm:text-center w-full h-[150px] flex items-center justify-center z-0 hover:z-50 ">
       <div className="w-full bg-white opacity-50 rounded-lg m-5 p-5">
         <p>
-          I'm Amber Srivastava, a frontend developer with expertise in HTML, CSS, and
+          I&apos;m Amber Srivastava, a frontend developer with expertise in HTML, CSS, and
           JavaScript. I love creating captivating websites with a focus on user
           experience. I enjoy collaborating with others to turn ideas into
-          dynamic interfaces. Let's work together and build something amazing!
+          dynamic interfaces. Let&apos;s work together and build something amazing!
         </p>
       </div>
     </div>
